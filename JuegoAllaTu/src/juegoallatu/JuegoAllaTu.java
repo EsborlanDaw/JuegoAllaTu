66e6bd5750002856ddb7053d5ef174913df49640
bolaes <bolaes@alumni.uv.es>
2021-12-08 18:08:00 +0100

    Avance juego: Intentando almacenar los datos de los maletines y eliminar los escogidos

--- JuegoAllaTu/src/juegoallatu/JuegoAllaTu.java
+++ JuegoAllaTu/src/juegoallatu/JuegoAllaTu.java
@@ -4,49 +4,220 @@
  */
 package juegoallatu;
 
+import java.util.Scanner;
+
 /**
  *
  * @author fani6
  */
 public class JuegoAllaTu {
-    //Asignacion nombres de cada maletin
-    String [] nMaletin = {"Maletin 1","Maletin 2","Maletin 3","Maletin 4","Maletin 5","Maletin 6","Maletin 7",
-    "Maletin 8","Maletin 9","Maletin 10","Maletin 11","Maletin 12","Maletin 13","Maletin 14","Maletin 15","Maletin 16",
-    "Maletin 17","Maletin 18","Maletin 19","Maletin 20","Maletin 21","Maletin 22"};
-    //Asignacion canntidad de dinero de cada maletin
-    double [] dMaeltin = {0.10,600000,50,1000,50000,100,1,400,200000,0.50,4000,700,20,30000,7500,8000,2000,6000,25,50000,450,100000};
-    //Asignacion preguntas
-    String [] preguntasAleatorias = {"¿Cuál es el deporte en el que destacó Carl Lewis?",
-        "¿De qué país forma parte Hawaii?","¿Quién es el autor de La Odisea",
-        "¿De qué grupo es la canción Let It Be?","¿Cuál es la lengua originaria del castellano?"
-            ,"¿Quién es considerado como el Rey del Rock?","¿Cada cuántos años se realizan los Juegos Olímpicos?"
-            ,"¿Quién fue el primer presidente de Estados Unidos?"," ¿Qué filósofo de la Antigua Grecia creía que"
-            + "el elemento del que están compuestas todas las cosas es el agua?"," ¿Cuál es el río más caudaloso del mundo?",
-            "¿Cuál es el monte más alto del mundo?","¿Cuál es el país de mayor tamaño del mundo?","¿Qué conocido cómico imitó a Hitler en la película El Gran Dictador"
-            ,"¿Quién fue el famoso cantante del grupo musical Queen?"," ¿Cómo se llama la ciudad donde se encuentra el Mago de Oz?"
-            ,"¿Por qué película superventas de los años 90 es conocido el director y productor James Cameron?",
-            "¿Cómo se llama la protagonista de la saga de videojuegos The Legend of Zelda",
-            "¿A qué saga de películas pertenece el personaje conocido como Jack Sparrow?",
-            "¿Cómo se llama el líder de los Autobots en Transformers","¿Qué gran artista es conocido por haber pintado la Capilla Sixtina?",
-            "¿De qué estilo arquitectónico es la catedral de Notre Dame?","¿De qué obra de Shakespeare forma parte el soliloquio Ser o no ser, esa es la cuestión",
-            "¿Qué nombre tenía el caballo de Don Quijote de la Mancha?","¿De qué país es originario el tipo de poesía conocido como haiku?"};
-    String [] respuestasAleatorias = {"Atletismo","Estados Unidos","Homero","The Beatles","Latin"
-            ,"Elvis Presley","4 años","George Washington","Tales de Mileto","Amazonas","Everest","Rusia"
-            ,"Charles Chaplin","Freddie Mercury","Ciudad Esmeralda","Titanic","Link","Piratas del Caribe","Japón"};
-      
-      public static int generarNumerosAleatorios(int min, int max) {
-          
+
+    //Variable global para leer las respuestas introducidas por el ussuario
+    public static Scanner cin = new Scanner(System.in);
+
+    public static void mostrarIntroduccion() {
+        System.out.println("Bienvenidos y bienvenidas a Allá Tú. Intenta llevarte 2000000 millones" + "\n"
+                + "de euros. Al inicio del juego tendrás la opción de elegir uno de nuestros 15 maletines." + "\n"
+                + "Cada uno tiene entre 0,10 cent a 2000000 de euros. Para tener la opcion de escoger otro" + "\n"
+                + "solo tendrás que acertar alguna de nuestras preguntas aleatorias. Pero, eso sí, si fallas" + "\n"
+                + "tendrás que eliminar alguno de los maletines. Mucha suerte y que comience el juego." + "\n"
+                + "Pulsa INTRO.");
+        cin.nextLine();
+        asignacionMaletines(true);
+    }
+
+    //Funcion que pide la respuesta del usuario en formato String
+    public static String pedirRespuestaString() {
+
+        System.out.println("Introduce respuesta: ");
+        String res = cin.nextLine();
+
+        return res;
+    }
+
+    //Funcion que pide la respuesta del ususario en formato Int
+    public static int pedirRespuestaInt() {
+
+        System.out.println("Introduce respuesta: ");
+        int res = cin.nextInt();
+
+        return res;
+    }
+
+    //Funcion que almacena los datos del maletin, además de ralizar la operacionde cambio o borrado de maletin
+    public static void asignacionMaletines(boolean acierto) {
+
+        //Asignacion nombres de cada maletin
+        String[] nMaletin = {"Maletin 1", "Maletin 2", "Maletin 3", "Maletin 4", "Maletin 5", "Maletin 6", "Maletin 7",
+            "Maletin 8", "Maletin 9", "Maletin 10", "Maletin 11", "Maletin 12", "Maletin 13", "Maletin 14", "Maletin 15"};
+
+        //Asignacion dinero de cada maletin
+        double[] dMaletin = {0.10, 600000, 50, 1000000, 50000, 100, 1, 400, 200000, 4000, 700, 20, 30000, 7500, 8000};
+
+        //Variable inicializada con el numero de datos de cada vector, que se usará para borrar datos de ambos vectores
+        int totalMaletines = 15;
+
+        //Variables para almacenar los maletines escpgidos por el teclado
+        String maletinUsuario;
+        double dmaletinUsuario;
+
+        //Variable entera que indicará el numero de maletin que quiere coger o eliminar el usuario
+        int nMal;
+
+        //A raiz de si el ususario ha acertado o no la pregunta se llevará a cabo el cambio de maletin o la eliminacion de uno de ellos
+        //Respuesta acertada
+        if (acierto == true) {
+            System.out.println("Indica que maletin quieres: ");
+            nMal = pedirRespuestaInt();
+            nMal = nMal - 1;
+
+            maletinUsuario = nMaletin[nMal];
+            dmaletinUsuario = dMaletin[nMal];
+
+            System.out.println(" Tu maletin es: " + maletinUsuario);
+
+        } //Respuesta errónea
+        else {
+            System.out.println("Indica que maletin quieres borrar: ");
+            nMal = pedirRespuestaInt();
+            nMal = nMal - 1;
+
+            System.out.println("El maletin que has borrado es: " + nMaletin[nMal]
+                    + " y contenia " + dMaletin[nMal]);
+        }
+
+        //Siendo i = Maletin que quiere el ususario y menor que el tamaño del vector
+        for (int i = nMal; i < totalMaletines - 1; i++) {
+            //Se irá intercambiando de atrás hacía delante hasta llevar el eliminado al final del vector
+            nMaletin[i] = nMaletin[i + 1];
+            dMaletin[i] = dMaletin[i + 1];
+        }
+
+        System.out.println("Ahora quedan: ");
+
+        //Mostramos los maletines que quedan
+        for (int i = 0; i < totalMaletines; i++) {
+            System.out.print(" , " + nMaletin[i]);
+            System.out.println(" ");
+        }
+
+        totalMaletines--;
+        generarPreguntasyRespuestasAleatorias();
+
+    }
+
+    //Funcion que genera una posicion aleatoria para generar preguntas aleatorias
+    public static int generarNumerosAleatorios(int min, int max) {
+
         int pos = ((int) (min + Math.random() * (max - min + min)));
-        
+
         return pos;
     }
-      public static void generarPreguntasAleatorias(int pos)
-      {
-          
-      }
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    //Funcion donde se inicializan las preguntas aleatorias con sus respectivas respuestas
+    public static String generarPreguntasyRespuestasAleatorias() {
+        //Vector de preguntas aleatorias
+        String[] preguntasAleatorias = {"¿Cuál es el deporte en el que destacó Carl Lewis?",
+            "¿De qué país forma parte Hawaii?", "¿Quién es el autor de La Odisea",
+            "¿De qué grupo es la canción Let It Be?", "¿Cuál es la lengua originaria del castellano?",
+            "¿Quién es considerado como el Rey del Rock?", "¿Cada cuántos años se realizan los Juegos Olímpicos?",
+            "¿Quién fue el primer presidente de Estados Unidos?", " ¿Qué filósofo de la Antigua Grecia creía que"
+            + "el elemento del que están compuestas todas las cosas es el agua?", " ¿Cuál es el río más caudaloso del mundo?",
+            "¿Cuál es el monte más alto del mundo?", "¿Cuál es el país de mayor tamaño del mundo?", "¿Qué conocido cómico imitó a Hitler en la película El Gran Dictador",
+            "¿Quién fue el famoso cantante del grupo musical Queen?", " ¿Cómo se llama la ciudad donde se encuentra el Mago de Oz?",
+            "¿Por qué película superventas de los años 90 es conocido el director y productor James Cameron?",
+            "¿Cómo se llama la protagonista de la saga de videojuegos The Legend of Zelda?",
+            "¿A qué saga de películas pertenece el personaje conocido como Jack Sparrow?",
+            "¿Cómo se llama el líder de los Autobots en Transformers?", "¿Qué gran artista es conocido por haber pintado la Capilla Sixtina?",
+            "¿De qué estilo arquitectónico es la catedral de Notre Dame?", "¿De qué obra de Shakespeare forma parte el soliloquio Ser o no ser, esa es la cuestión",
+            "¿Qué nombre tenía el caballo de Don Quijote de la Mancha?", "¿De qué país es originario el tipo de poesía conocido como haiku?",
+            "¿Qué pais está entre Perú y Colombia?", "¿A qué país pertenece la isla de Creta?", "¿Qué batalla crucial tuvo lugar en 1815?",
+            "¿Cuál era el famodo lema de los tres mosqueteros?", "¿En qué mes el sol está más cerca de la tierra?", "¿Qué pieza del ajedrez puede hacer"
+            + "un movimiento en forma de L?", "¿A qué equipo de fútbol pertenecía el estadio Metropolitano?", "¿En qué serie se encuntra un personaje"
+            + "llamado Walter White?"};
+        //Vector de respuestas aleatorias
+        String[] respuestasAleatorias = {"Atletismo", "Estados Unidos", "Homero", "The Beatles", "Latin",
+            "Elvis Presley", "4 años", "George Washington", "Tales de Mileto", "Amazonas", "Everest", "Rusia",
+            "Charles Chaplin", "Freddie Mercury", "Ciudad Esmeralda", "Titanic", "Link", "Piratas del Caribe", "Optimus Prime", "Miguel Angel", "Gótico",
+            "Hamlet", "Rocinante", "Japón", "Ecuador", "Grecia", "Waterloo", "Todos para uno y uno para todos", "Diciembre", "Caballo", "Atlético de Madrid",
+             "Breaking Bad"};
+
+        //Variable en la que se almacena la pregunta y respuesta seleccionada
+        String pregunta, respuesta;
+        //Variable que nos facilitará la eliminacion de las preguntas ya seleccionada para evitar así que se repitan
+        int totalpreguntas = 32;
+        //Pedimos la posicion de la pregunta aleatoria
+        int pos = generarNumerosAleatorios(0, totalpreguntas);
+        //Asignamos a las variables la pregunta y respuesta seleccionada
+
+        pregunta = preguntasAleatorias[pos];
+        respuesta = respuestasAleatorias[pos];
+
+        //Procedemos a retirar esa pregunta y respuesta del vector
+        for (int i = pos; i < totalpreguntas - 1; i++) {
+            preguntasAleatorias[i] = preguntasAleatorias[i + 1];
+            respuestasAleatorias[i] = respuestasAleatorias[i + 1];
+        }
+        //Deducimos el tamaño del vector
+        totalpreguntas--;
+
+        //La mostramos
+        System.out.println(pregunta);
+        cin.nextLine();
+
+        //Devolvemos la respuesta para comprobar si es correcta
+        String resusuario = pedirRespuestaString();
+        comprobarRespuesta(respuesta, resusuario);
+
+        return respuesta;
+
+    }
+
+    //Funcion que comprueba si la respuesta es o no correcto, le introducimos la respuesta correcta
+    //y la respuesta elegida por el ususario
+    public static void comprobarRespuesta(String respuesta, String resusuario) {
+        boolean acierto = true;
+
+        if (respuesta.equalsIgnoreCase(resusuario) == acierto) {
+            System.out.println("Respuesta: CORRECTA");
+            System.out.println("Ahora tienes dos opciones: ");
+            System.out.println("1.Cambiar de maletin.");
+            System.out.println("2.Pasar a la siguiente pregunta.");
+            int op = cin.nextInt();
+            if (op == 1) {
+                asignacionMaletines(acierto);
+            } else {
+                generarPreguntasyRespuestasAleatorias();
+            }
+
+        } else {
+
+            acierto = false;
+            asignacionMaletines(acierto);
+        }
+
     }
     
+    public static void mostrarFindeJuego(String nMaletin, double dMaletin)
+    {
+        System.out.println("Hemos llegado al final del juego.Bien jugado, vamos a ver cuánto"
+                + "has ganado: ");
+        
+        
+    }
+
+    public static void main(String[] args) {
+
+        mostrarIntroduccion();
+
+        int veces = 15;
+
+        do {
+            generarPreguntasyRespuestasAleatorias();
+            veces--;
+
+        } while (veces <= 15);
+        
+
+    }
 }
